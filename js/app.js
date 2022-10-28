e37cfd8e7ea57f45520708e493d47f67b426a037
Hadi14 <asusn43@yahoo.com>
2022-10-28 15:44:14 +0330

    ok

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 // DOM => Document Object Model
 // BOM => Browser Object Model
+const pc = document.querySelector('.container');
+const products = [
+    { id: 1, title: "Sport Shoo", price: 20 },
+    { id: 2, title: "Women Shoo", price: 25 },
+    { id: 3, title: "Boost", price: 15 }
+]
+
+products.forEach((pr) => {
+    let tag = ' <div class="product-card">' +
+        '<h1>' + pr.title + '</h1>' +
+        '<p>Lorem ipsum, or lipsum as it is sometimes known</p>' +
+        '<div class="product-pic" style="background-image: url(images/' + pr.id + '.png);"></div>' +
+        '<div class="product-info">' +
+        '<div class="product-price">$' + pr.price + '</div>' +
+        '<a href="product.html?id=' + pr.id + '" class="product-button">See More</a></div></div>';
+    pc.insertAdjacentHTML('beforeend', tag);
+})
--- js/product.js
+++ js/product.js
@@ -1,1 +1,19 @@
 // ✌ :))
+const h1 = document.querySelector('h1');
+const ptag = document.querySelector('p');
+const img = document.querySelector('img');
+const products = [
+    { id: 1, title: "Sport Shoo", price: 20 },
+    { id: 2, title: "Women Shoo", price: 25 },
+    { id: 3, title: "Boost", price: 15 }
+]
+
+let s = new URLSearchParams(location.search);
+let id = s.get('id');
+let x = products.find((item) => {
+    return item.id == id;
+})
+// console.log(x)
+h1.innerHTML = x.title;
+// ptag.innerHTML=x.
+img.src = ('./images/' + x.id + '.png')
